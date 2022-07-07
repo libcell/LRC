5a96cfd3c659c99852b7f7ec3d7367fe1a22790a
Bo Lee <libcell@cqnu.edu.cn>
2022-07-07 15:36:36 +0800

    added

--- Lecture-03_Objects_in_R_Language_part_2.R
+++ Lecture-03_Objects_in_R_Language_part_2.R
@@ -43,22 +43,94 @@ setwd("D:/00-GitHub/LRC/tmp/")
 ### ****************************************************************************
 ### Step-05. About matrix in R. 
 
-# 1) Generating matrix.  
+# 1) Generating matrix. 
+
+# Elements are arranged sequentially by row.
+M <- matrix(c(3:14), nrow = 4, byrow = TRUE)
+print(M)
+
+# Elements are arranged sequentially by column.
+N <- matrix(c(3:14), nrow = 4, byrow = FALSE)
+print(N)
+
+# Define the column and row names.
+rownames = c("row1", "row2", "row3", "row4")
+colnames = c("col1", "col2", "col3")
+
+P <- matrix(c(3:14), nrow = 4, byrow = TRUE, dimnames = list(rownames, colnames))
+print(P)
+
 # 2) Accessing the matrix. 
 
+# Define the column and row names.
+rownames = c("row1", "row2", "row3", "row4")
+colnames = c("col1", "col2", "col3")
+
+# Create the matrix.
+P <- matrix(c(3:14), nrow = 4, byrow = TRUE, dimnames = list(rownames, colnames))
+
+# Access the element at 3rd column and 1st row.
+print(P[1,3])
+
+# Access the element at 2nd column and 4th row.
+print(P[4,2])
+
+# Access only the  2nd row.
+print(P[2,])
+
+# Access only the 3rd column.
+print(P[,3])
+
+### ****************************************************************************
+### Step-06. About array in R. 
+
+# 1) Generating array.  
+
+# Create two vectors of different lengths.
+vector1 <- c(5, 9, 3)
+vector2 <- c(10, 11, 12, 13, 14, 15)
+
+# Take these vectors as input to the array.
+result <- array(c(vector1, vector2), dim = c(3, 3, 2))
+print(result)
+
+# 2) Naming Columns and Rows. 
+
+# Create two vectors of different lengths.
+vector1 <- c(5, 9, 3)
+vector2 <- c(10, 11, 12, 13, 14, 15)
+column.names <- c("COL1", "COL2", "COL3")
+row.names <- c("ROW1", "ROW2", "ROW3")
+matrix.names <- c("Matrix1", "Matrix2")
+
+# Take these vectors as input to the array.
+result <- array(c(vector1,vector2),dim = c(3,3,2),dimnames = list(row.names,column.names,
+                                                                  matrix.names))
+print(result)
 
 
+# 3) Accessing array elements. 
 
+# Create two vectors of different lengths.
+vector1 <- c(5,9,3)
+vector2 <- c(10,11,12,13,14,15)
+column.names <- c("COL1","COL2","COL3")
+row.names <- c("ROW1","ROW2","ROW3")
+matrix.names <- c("Matrix1","Matrix2")
 
+# Take these vectors as input to the array.
+result <- array(c(vector1,vector2),dim = c(3,3,2),dimnames = list(row.names,
+                                                                  column.names, matrix.names))
 
+# Print the third row of the second matrix of the array.
+print(result[3,,2])
 
+# Print the element in the 1st row and 3rd column of the 1st matrix.
+print(result[1,3,1])
 
+# Print the 2nd Matrix.
+print(result[,,2])
 
-
-
-
-
-
-
+# 4) Image and array. 
 
 
