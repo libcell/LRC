6daa7b178f13dc3e072ac2b7de66997b125dcb7a
Bo Lee <libcell@cqnu.edu.cn>
2022-07-12 21:26:40 +0800

    repalced

--- Lecture-05 Importing and Exporting the files in R Envrionment.R
+++ Lecture-05 Importing and Exporting the files in R Envrionment.R
@@ -30,8 +30,17 @@ setwd("D:/00-GitHub/LRC/tmp/")
 
 # 1) Reading the CSV files. 
 
+x <- read.csv("iris.csv", header = TRUE)
+print(x)
 
 # 2) Writing the CSV files. 
+
+dir()
+
+# write.csv(iris, "iris.csv")
+write.csv(x = iris, file = "iris.csv")
+
+dir()
 
 ### End of Step-02.
 ### ****************************************************************************
@@ -41,8 +50,13 @@ setwd("D:/00-GitHub/LRC/tmp/")
 
 # 1) Reading the TXT files. 
 
+y <- read.table("iris.csv", sep = ",", header = TRUE)
+
+print(y)
 
 # 2) Writing the TXT files. 
+
+write.table(y, "newiris.txt")
 
 ### End of Step-03.
 ### ****************************************************************************
@@ -50,10 +64,17 @@ setwd("D:/00-GitHub/LRC/tmp/")
 ### ****************************************************************************
 ### Step-04. Reading and writing xlsx format files. 
 
+library(openxlsx)
+
 # 1) Reading the xlsx files. 
 
+z <- read.xlsx("newiris2.xlsx", sheet = 1)
+
+head(z)
 
 # 2) Writing the xlsx files. 
+
+write.xlsx(iris, file = "newiris2.xlsx")
 
 ### End of Step-04.
 ### ****************************************************************************
@@ -61,8 +82,23 @@ setwd("D:/00-GitHub/LRC/tmp/")
 ### ****************************************************************************
 ### Step-05. Reading and writing XML format files. 
 
+library(RCurl)
+library(XML)
+
 # 1) Reading the XML files. 
 
+library(XML)
+library(RCurl)
+url <- "http://www.knapsackfamily.com/knapsack_core/information.php?sname=C_ID&word=C00000001"
+content <- getURL(url, .encoding = character())
+tables <- readHTMLTable(content)
+length(tables)
+class(tables)
+tables[[1]]
+tables[[2]]
+metaDB <- tables[[3]]
+
+dim(metaDB)
 
 # 2) Writing the XML files. 
 
@@ -73,7 +109,19 @@ setwd("D:/00-GitHub/LRC/tmp/")
 ### Step-06. Reading and writing json format files. 
 
 # 1) Reading the json files. 
+install.packages("rjson")
+library(rjson)
+content <- fromJSON("test.json")
 
+save(iris, "iris.RData")
+save(iris, file = "iris.RData")
+
+content <- fromJSON(file = "test.json")
+
+
+#-------------- testing this error!-------------------------------
+
+write(content, file = "newdata.json") # !!! 
 
 # 2) Writing the json files. 
 
