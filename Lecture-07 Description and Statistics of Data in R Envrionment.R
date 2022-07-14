5f66785a6ff95eb3d1d11fdade44dbd5ce1d2c30
Bo Lee <libcell@cqnu.edu.cn>
2022-07-14 20:16:33 +0800

    Update Lecture-07 Description and Statistics of Data in R Envrionment.R

--- Lecture-07 Description and Statistics of Data in R Envrionment.R
+++ Lecture-07 Description and Statistics of Data in R Envrionment.R
@@ -30,16 +30,71 @@ setwd("D:/00-GitHub/LRC/tmp/")
 
 # 1) Simulating a GEM showing expression levels of 2000 genes in 100 samples. 
 
+gem <- rnorm(2000*100, mean = 8, sd = 2)
+
+length(gem)
+
+is.matrix(gem)
+
+gem <- matrix(gem, nrow = 2000)
+
+dim(gem)
+
+head(gem)
+
+rownames(gem) <- paste("mRNA", 1:2000, sep = "-")
+
+colnames(gem) <- paste("sample", 1:100, sep = "-")
+
+head(gem)
+
+# 2) Checking the data distribution.  
+
+# 3) Computing the general statistics. 
+
+gem["mRNA-1", ]
+
+gem[, "sample-1"]
+
+gem["mRNA-1", "sample-1"]
+
+gem[1, 1]
+
+# 1) average value of gene-1. 
+
+mean(gem[1, ])
+mean(gem["mRNA-1", ])
+
+range(gem["mRNA-1", ])
+
+summary(gem["mRNA-1", ])
+
+# 2) correlation
+
+r.value <- cor(gem)
+
+dim(r.value)
+
+head(r.value)
+
+cov(gem)
+
+# 4) Normalizing the data. 
+
+library(DT)
+
+datatable(gem)
+
+datatable(iris)
+
+boxplot(iris[, -5], col = 2:5)
+
+boxplot(scale(iris[, -5]), col = 2:5)
 
 
-# 2) . 
 
-# 3) 
 
-# 4) 
-
-# 5) data distribution
-
+# 5) Visualizing the data. 
 
 ### End of Step-02.
 ### ****************************************************************************
@@ -58,8 +113,6 @@ skewness(iris[, -5])
 
 kurtosis(iris[, -5])
 
-
-
 # 2) Computing the statistics.
 
 
@@ -67,19 +120,19 @@ kurtosis(iris[, -5])
 ### ****************************************************************************
 
 ### ****************************************************************************
-### Step-04. Reading and writing xlsx format files. 
+### Step-04. Understanding the types of data. 
 
-# 1) . 
+# 1) Continuous data 
 
 
-# 2) . 
+# 2) Discrete data 
 
 
 ### End of Step-04.
 ### ****************************************************************************
 
 ### ****************************************************************************
-### Step-05. . 
+### Step-05. Data visualization of gene expression data.  
 
 # 1) Reading the XML files. 
 
@@ -87,17 +140,6 @@ kurtosis(iris[, -5])
 # 2) Writing the XML files. 
 
 ### End of Step-05.
-### ****************************************************************************
-
-### ****************************************************************************
-### Step-06. . 
-
-# 1) Reading the json files. 
-
-
-# 2) Writing the json files. 
-
-### End of Step-06.
 ### ****************************************************************************
 
 ################################################################################
