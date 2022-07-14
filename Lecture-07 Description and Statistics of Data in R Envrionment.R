30301a3760e9e45c7ec046bca18530b4f25af860
Bo Lee <libcell@cqnu.edu.cn>
2022-07-14 21:17:06 +0800

    Update Lecture-07 Description and Statistics of Data in R Envrionment.R

--- Lecture-07 Description and Statistics of Data in R Envrionment.R
+++ Lecture-07 Description and Statistics of Data in R Envrionment.R
@@ -91,8 +91,54 @@ boxplot(iris[, -5], col = 2:5)
 
 boxplot(scale(iris[, -5]), col = 2:5)
 
+# centering, 
 
+S1 <- runif(26, min = 20, max = 80) # random sampling 
+S1 <- round(S1)
+names(S1) <- LETTERS
+print(S1)
 
+S2 <- runif(20, min = 50, max = 100) # random sampling 
+S2 <- round(S2)
+names(S2) <- letters[1:20]
+print(S2)
+
+mean(S1)
+mean(S2)
+
+S1
+S2
+
+# D vs. d
+
+D <- 68
+d <- 77
+
+op <- par(mfrow = c(1, 2))
+boxplot(S1)
+boxplot(S2)
+par(op)
+
+D1 <- 68 - mean(S1)
+d1 <- 77 - mean(S2)
+
+D1
+d1
+
+D2 <- (68 - mean(S1))/sd(S1)
+d2 <- (77 - mean(S2))/sd(S2)
+D2
+d2
+
+# 归一化
+
+D3 <- ((68 - min(S1))/(max(S1) - min(S1)))*100 
+d3 <- ((77 - min(S2))/(max(S2) - min(S2)))*100 
+
+D3
+d3
+
+# S1
 
 # 5) Visualizing the data. 
 
@@ -118,6 +164,35 @@ kurtosis(iris[, -5])
 
 ### End of Step-03.
 ### ****************************************************************************
+
+
+
+dim(gem)
+
+g1 <- gem[1, ]
+g2 <- gem[2, ]
+g3 <- gem[3, ]
+g4 <- gem[4, ]
+
+hist(gem, breaks = 20, probability = TRUE, ylim = c(0, .25))
+
+lines(density(g1), col = "red", lwd = 3)
+lines(density(g2), col = "green", lwd = 3)
+lines(density(g3), col = "blue", lwd = 3)
+
+hist(gem, breaks = 20, probability = TRUE, ylim = c(0, .25))
+
+for (i in 1:10) {
+  lines(density(gem[i, ]), col = rainbow(10)[i], lwd = 2)
+}
+
+
+boxplot(gem[, 1:10], col = 1:10)
+
+
+# par(mfrow = c(1, 1))
+
+
 
 ### ****************************************************************************
 ### Step-04. Understanding the types of data. 
